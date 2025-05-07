f7bd45215590703f4b16b91d0f116edfc7a6bbc7
mohaideenkhadar <mohaideenabdulkhadar006@gmail.com>
2025-05-07 13:04:53 +0530

    Made some changes

--- client/src/auth.js
+++ client/src/auth.js
@@ -1,32 +1,47 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google"
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google({
-        clientId: process.env.AUTH_GOOGLE_ID,
-        clientSecret: process.env.AUTH_GOOGLE_SECRET,
-        authorization: {
-            params: {
-                prompt: "consent",
-                access_type: "offline",
-                response_type: "code"
-            }
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
         }
-    })],
-    secret: process.env.AUTH_SECRET,
-    callbacks: {
-        async jwt({token, account}) {
-            if(account?.id_token) {
-                token.idToken = account.id_token;
-            }
-            return token;
-        },
-        async session({session, token}) {
-            session.idToken = token.idToken;
-            return session;
-        },
-    },
-    pages: {
-        error: '/login', // Redirect to login on error
+      }
+    })
+  ],
+  secret: process.env.AUTH_SECRET,
+  trustHost: true, // Required for Netlify
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      }
     }
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account?.id_token) {
+        token.idToken = account.id_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.idToken = token.idToken;
+      return session;
+    }
+  },
+  pages: {
+    signIn: '/login',
+    error: '/login'
+  }
 });
